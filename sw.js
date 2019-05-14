fef01be722db63a48a25cec78b580bc49bb50956
Fred-Jacobs <AzureDevOps+Fred-Jacobs@users.noreply.github.com>
2019-05-14 12:07:17 +0000

    Automated Release 9

--- sw.js
+++ sw.js
@@ -6,16 +6,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "84a0b0f0f156f4887f0394fcd7049770"
   },
   {
+    "url": "/_nuxt/487320d1215eaa123fae.js",
+    "revision": "8217fb39e767f8e3ef501184b555de2b"
+  },
+  {
     "url": "/_nuxt/49e9ffeb3e2098b9bb86.js",
     "revision": "cbf92206920e880befeaba5a2c9688b7"
   },
   {
     "url": "/_nuxt/97fc374bd3bd6b7c8a74.js",
     "revision": "06971301780b48d32832df915418c74d"
-  },
-  {
-    "url": "/_nuxt/b081ba899de2b5a5f3dc.js",
-    "revision": "298a5f036dc1d5a678cebff44a177603"
   },
   {
     "url": "/_nuxt/bd607427522869f94cfa.js",
@@ -26,16 +26,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "4e44254f19f01c85dc584f2fefd10593"
   },
   {
+    "url": "/_nuxt/bf2b20669ba411f89a4b.js",
+    "revision": "423a4f8147e9c500f5e974df2f4737a7"
+  },
+  {
     "url": "/_nuxt/e58494a4664f2b719860.js",
     "revision": "fe3a3d53883149fcf432e66d12c5187a"
   },
   {
     "url": "/_nuxt/ec33b7da458e3a700047.js",
     "revision": "89fd831e0f0fc2ddf603c3eb498bca01"
-  },
-  {
-    "url": "/_nuxt/edba4fdec0b37dbf1c04.js",
-    "revision": "3cc774531c56f2f146a995a0735de514"
   }
 ], {
   "cacheId": "fred-jacobs.github.io",
